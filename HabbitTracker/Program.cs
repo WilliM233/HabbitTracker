ceac3fa0c02ddc214d3da5fe689fb4c4d41021c2
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2023-10-27 09:22:11 -0500

    Create and display main menu, accept only valid input

--- HabbitTracker/Program.cs
+++ HabbitTracker/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Display Main Menu and Process Input
+DisplayMenu();
+ProcessInput();
+
+
+
+void DisplayMenu()
+{
+    Console.WriteLine("MAIN MENU");
+    Console.WriteLine();
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine();
+    Console.WriteLine("Type 0 to Close Application.");
+    Console.WriteLine("Type 1 to View All Records.");
+    Console.WriteLine("Type 2 to Insert Record.");
+    Console.WriteLine("Type 3 to Delete Record.");
+    Console.WriteLine("Type 4 to Update Record.");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine();
+}
+
+void ProcessInput()
+{
+    string? input = Console.ReadLine();
+    switch(input)
+    {
+        case "0":
+            Environment.Exit(0);
+            break;
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        default:
+            Console.WriteLine("Invalid Input, enter valid number");
+            ProcessInput();
+            break;
+    }
+}
